f7f6b9f7056ce53384cb0275193369ef27cfbbfd
Nafis Ahmed <102437433+ahmad-cit22@users.noreply.github.com>
2022-05-10 06:14:02 +0600

    add

---
+++ ps1.js
@@ -0,0 +1,11 @@
+//! Codeforces problem solving : 1 (watermelon)
+
+// var w = parseInt(readline());
+//     if (w > 2 && w % 2 === 0) {
+//         console.log ("YES");
+//     } else {
+//        console.log ("NO");
+//     }
+
+
+
